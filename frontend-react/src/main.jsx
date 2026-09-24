19b676e86650755aba4117d5843ca6ce432f6be0
Ayush Gupta <145848125+ayush2459@users.noreply.github.com>
2026-09-24 11:20:18 +0530

    Add FlowSense auth provider

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { FlowSenseProvider } from "./context/FlowSenseContext";
+import { AuthProvider } from "./AuthContext";
 import "./index.css";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <FlowSenseProvider>
+    <AuthProvider>
       <App />
-    </FlowSenseProvider>
+    </AuthProvider>
   </BrowserRouter>
 );
